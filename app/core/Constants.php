d09b24fced6f41494e34cae41cc79471d8d89ea0
asanoerdotcom <kendalrejo1986@yahoo.co.id>
2025-02-02 22:04:15 +0700

    EPISODE 6, MENGELOLA ASSET DAN AKSESNYA

---
+++ app/core/Constants.php
@@ -0,0 +1,4 @@
+<?php
+
+
+define('BASEURL', 'https://localhost/mvc-oop/public/');
--- app/views/About/index.php
+++ app/views/About/index.php
@@ -1,2 +1,7 @@
-<h1>Halaman index About</h1>
-<p>Halo Saya adalah <?= $data['nama']; ?> merupakan seorang <?= $data['pekerjaan']; ?>.</p>
+<div class="container mt-5">
+    <div class="jumbotron">
+        <h1>Halaman index About</h1>
+        <img src="<?= BASEURL; ?>img/my-foto.jpg" alt="Khasan" width="150" class="rounded-circle shadow" />
+        <p>Halo Saya adalah <?= $data['nama']; ?> merupakan seorang <?= $data['pekerjaan']; ?>.</p>
+    </div>
+</div>
