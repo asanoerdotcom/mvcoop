23e817335459f8424b53874e4dbe3a4a86f19680
asanoerdotcom <kendalrejo1986@yahoo.co.id>
2025-02-03 21:17:34 +0700

    EPISODE 7-B, MEMBUAT MODEL DENGAN DATA DARI DATABASE MENGGUNAKAN PDO

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -2,28 +2,24 @@
 
 class Mahasiswa_model
 {
-    private $mhs = [
-        [
-            "nama" => "Rizky Khapidsyah",
-            "nim" => "L200180012",
-            "email" => "rizkykhapidsyah@gmail.com",
-            "jurusan" => "Teknik Informatika"
-        ],
-        [
-            "nama" => "Miftahudin",
-            "nim" => "L200184569",
-            "email" => "Miftahudin@gmail.com",
-            "jurusan" => "Ilmu Jaringan"
-        ],
-        [
-            "nama" => "Siska Oktapia",
-            "nim" => "L200185869",
-            "email" => "s1sk4@gmail.com",
-            "jurusan" => "Farmasi Teknikal"
-        ]
-    ];
+    private $dbh; // database handler
+    private $stmt; // statement
+
+    public function __construct()
+    {
+        // $dsn adalah data source name
+        $dsn = 'mysql:host=localhost;dbname=mvc-oop';
+
+        try {
+            $this->dbh = new PDO($dsn, 'root', '');
+        } catch (PDOException $e) { // Ambil eror dan masukkan ke dalam variable $e
+            die($e->getMessage()); // jika terjadi kesalahan, maka akan menampilkan pesan kesalahan
+        }
+    }
     public function getAllMahasiswa()
     {
-        return $this->mhs;
+        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+        $this->stmt->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
